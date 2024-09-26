b762ec13cbf0c43a728e8f43a31922d7c45fc0a9
Admin <mr.mr.sakolov@mail.ru>
2024-09-26 20:30:49 +0300

    Домашнее задание №3 Условный-оператор

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Домашнее задание №3 Условный оператор");
+        System.out.println("Домашнее задание №3 Условный оператор ");
 
 
 
